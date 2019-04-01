663b461652f312b50e7f7521925e258a2f633757
Andrew Kuipers <andrewk@iqmetrix.com>
2019-04-01 13:01:08 -0600

    Wired up ButtonPresenter

---
+++ Assets/Code/Infrastructure/SceneInstaller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Code.Model;
+using UnityEngine;
+using Zenject;
+
+public class SceneInstaller : MonoInstaller
+{
+	public override void InstallBindings()
+	{
+		Container.Bind<Game>().AsSingle();
+	}
+}
