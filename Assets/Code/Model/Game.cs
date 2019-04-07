24c8db3dc817cd6b8dfc08840aecead5df313f38
deimors <deimors@gmail.com>
2019-04-07 09:51:43 -0600

    XMarked / OMarked event conditional on boolean state [green]

--- Assets/Code/Model/Game.cs
+++ Assets/Code/Model/Game.cs
@@ -7,9 +7,17 @@ namespace Assets.Code.Model
 	{
 		private readonly ISubject<GameEvent> _events = new Subject<GameEvent>();
 		public IObservable<GameEvent> Events => _events;
+
+		private bool _isXTurn = true;
+
 		public void Mark(int x, int y)
 		{
-			_events.OnNext(new XMarkedEvent(x, y));
+			if (_isXTurn)
+				_events.OnNext(new XMarkedEvent(x, y));
+			else
+				_events.OnNext(new OMarkedEvent(x, y));
+
+			_isXTurn = !_isXTurn;
 		}
 	}
 }
