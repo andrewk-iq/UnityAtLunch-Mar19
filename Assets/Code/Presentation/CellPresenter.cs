3e9e96be4b6bf28a8f0b4b42f461e59118b30e32
Andrew Kuipers <andrewk@iqmetrix.com>
2019-04-08 12:24:52 -0600

    Added X and Y fields to CellPresenter, and using these in MarkX command, and reacting to XMarkedEvents

--- Assets/Code/Presentation/CellPresenter.cs
+++ Assets/Code/Presentation/CellPresenter.cs
@@ -11,13 +11,17 @@ public class CellPresenter : MonoBehaviour
 	public Button CellButton;
 	public Text CellText;
 
+	public int X;
+	public int Y;
+
 	[Inject]
 	public void Initialize(Game gameModel)
 	{
-		CellButton.onClick.AddListener(() => gameModel.MarkX(0, 0));
+		CellButton.onClick.AddListener(() => gameModel.MarkX(X, Y));
 
 		gameModel.Events
 			.OfType<GameEvent, XMarkedEvent>()
+			.Where(e => e.X == X && e.Y == Y)
 			.Subscribe(_ => CellText.text = "X");
 	}
 }
