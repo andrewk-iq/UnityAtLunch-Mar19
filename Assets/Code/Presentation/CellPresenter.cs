31c26fef01e72c3885fb9d0f00691f83500de4ba
Andrew Kuipers <andrewk@iqmetrix.com>
2019-04-08 12:20:14 -0600

    Renamed ButtonPresenter to CellPresenter, and implemented simple reaction to XMarkedEvent

---
+++ Assets/Code/Presentation/CellPresenter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Code.Model;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class CellPresenter : MonoBehaviour
+{
+	public Button CellButton;
+	public Text CellText;
+
+	[Inject]
+	public void Initialize(Game gameModel)
+	{
+		CellButton.onClick.AddListener(() => gameModel.MarkX(0, 0));
+
+		gameModel.Events
+			.OfType<GameEvent, XMarkedEvent>()
+			.Subscribe(_ => CellText.text = "X");
+	}
+}
